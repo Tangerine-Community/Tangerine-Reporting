a4e033f63a224d02aeb262278ea7e59df370413c
Mel Michael <mel.michael@andela.com>
2017-07-13 18:01:44 +0100

    Successfully query data to create sample result schema

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const chalk = require('chalk');
 const errorHandler = require('errorhandler');
+const _ = require('lodash');
+const Promise = require('bluebird');
 
 /**
  * Controllers (route handlers).
@@ -19,10 +21,19 @@ const app = express();
 
 
 /**
- * Connect to CouchDB.
+ * Connect to nanoDB.
  */
-const couch = require('nano')('http://localhost:5984');
-const TMP_TANGERINEDB = couch.db.use('tmp_tangerine');
+const nano = require('nano');
+
+// Using nano-promises library
+// const prom = require('nano-promises');
+// const TMP_TANGERINEDB = prom(nano('http://localhost:5984')).use('tmp_tangerine');
+
+// Using nano only
+const TMP_TANGERINEDB = nano('http://localhost:5984/tmp_tangerine');
+// const TMP_TANGERINEDB = nano('http://localhost:5984').use('tmp_tangerine');
+// Promise.promisifyAll(TMP_TANGERINEDB);
+
 
 /**
  * Express configuration.
@@ -35,6 +46,41 @@ app.use(bodyParser.urlencoded({ extended: false }));
 /**
  * API routes.
  */
+app.get('/', function(req, res) {
+  var columnData;
+  var allDB = [];
+  let columnHeaders;
+
+  // get a specific document
+  // TMP_TANGERINEDB
+  //  .get('0095F1B5-7A1D-7B1F-AC59-D2AF2FC05BFD', (err, body) => {
+  //     if (err) {
+  //        res.send(err);
+  //     }
+  //    columnHeaders = _.keysIn(body);
+  //    columnData = body;
+  //    res.json({ columnHeaders, columnData })
+  // });
+
+  TMP_TANGERINEDB.list({ include_docs: true }, (err, body) => {
+      if (err) return res.send(err);
+
+      // Get a single result collection document
+      let first  = _.find(body.rows, (data) => { return data.doc.collection === 'result'; });
+
+      // Get the keys for a result document. To be used as excel column headers
+      columnHeaders = _.keysIn(first.doc);
+
+      // Get all collections that are result
+      let resultCollections = _.filter(body.rows, (data) => data.doc.collection === 'result');
+
+      // Return just 10 documents of the data
+      res.json({ columnHeaders, results: resultCollections.slice(90, 100) })
+    })
+
+
+});
+
 app.get('/api/v1/assessment', assessmentController.get);
 
 /**
--- controllers/assessment.js
+++ controllers/assessment.js
@@ -1,12 +1,9 @@
 'use strict';
 const request = require('request');
 const Promise = require('bluebird');
-const couch = require('nano')('http://localhost:5984');
-const TMP_DB = couch.db.use('tmp_tangerine');
+const tmpDB = require('nano')('http://localhost:5984/tmp_tangerine');
 
 
 exports.get = (req, res) => {
-  console.log('in assessment');
-    res.send('In Assessment Route');
-
+  res.send('In Assessment Route');
 }
