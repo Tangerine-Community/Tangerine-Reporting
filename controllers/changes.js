9f555411e5961bfe24896d9584f312e08e62b603
Mel Michael <mel.michael@andela.com>
2017-08-17 19:27:29 +0100

    Implement data restructure upon changes in database

--- controllers/changes.js
+++ controllers/changes.js
@@ -9,52 +9,77 @@ const TMP_TANGERINE = nano('http://localhost:5984/tmp_tangerine');
 const RESULT_DB = nano('http://localhost:5984/tang_resultdb');
 const TAYARI_BACKUP = nano('http://localhost:5984/tayari_backup');
 
-const generateHeaders = require('./assessment').createColumnHeaders;
-const processResult = require('./result').generateResult;
-const processWorflowHeaders = require('./workflow').createWorkflowHeaders;
+const generateAssessmentHeaders = require('./assessment').createColumnHeaders;
+const processAssessmentResult = require('./result').generateResult;
+const generateWorkflowHeaders = require('./workflow').createWorkflowHeaders;
+const processWorkflowResult = require('./workflow').processWorkflowResult;
+
+
+// TODO: Remove sample doc before live data testing
+const sampleDoc =  {
+  "_id": "9adc36fa8cba4c7511a0bc3e7c00b",
+  "assessmentId": "a9468e01-6b3e-2fb5-1b47-2fa2cb1d4190",
+  "assessmentName": "During Obsevation Tool - Sub-County ECD Coordinator Observation",
+  "tripId": "a6aa892c-5c24-0be3-8fb4-628418150e6d",
+  "workflowId": "9e91640d-b308-38f6-eef5-baf17b4bafa8",
+  "subtestData": [],
+  "start_time": 1455689463600,
+  "enumerator": "stevenochieng",
+  "tscNumber": "233",
+  "tangerine_version": "1.7.1",
+  "instanceId": "aaaa-bbbb-cccc",
+  "editedBy": "stevenochieng",
+  "updated": "Wed Feb 17 2016 09:40:36 GMT+0300 (EAT)",
+  "hash": "w8Ec4SGYMIyZHrx0gcz4issM7hM=",
+  "fromInstanceId": "aaaa-bbbb-cccc",
+  "collection": "result"
+}
 
 /**
  * GET /tangerine_changes
  * process headers and result of changed document
 */
 exports.changes = (req, res) => {
-  let feed = TMP_TANGERINE.follow({since: 'now'});
-  feed.on('change', function (change) {
-    console.log('change: ', change);
+  const feed = TMP_TANGERINE.follow({ since: 'now', include_docs:true });
 
-    retrieveChangeDoc(change.id)
-      .then((data) => {
-        res.json(data);
-        if (data.collection === 'result') {
-          processResult(data.assessmentId);
-        }
-        if (data.collection === 'workflow') {
-          processWorflowHeaders(data.workflowId);
-        }
-        if (data.collection === 'assessment') {
-          generateHeaders(data.assessmentId);
-        }
-        if (data.collection === 'curriculum') {
-          generateHeaders(data.curriculumId);
-        }
-        if (data.collection === 'question') {
-          generateHeaders(data.assessmentId);
-        }
-        if (data.collection === 'subtest') {
-          generateHeaders(data.assessmentId);
-        }
-      })
-      .catch((err) => res.send(Error(err)));
+  feed.on('change', (resp) => {
+    let isWorkflowIdSet = (resp.doc.doc.workflowId) ? true : false;
+    let isResult = (resp.doc.doc.collection === 'result') ? true : false;
+    let isWorkflow = (resp.doc.doc.collection === 'workflow') ? true : false;
+    let isAssessment = (resp.doc.doc.collection === 'assessment') ? true : false;
+    let isCurriculum = (resp.doc.doc.collection === 'curriculum') ? true : false;
+    let isQuestion = (resp.doc.doc.collection === 'question') ? true : false;
+    let isSubtest = (resp.doc.doc.collection === 'subtest') ? true : false;
+
+    if (isWorkflowIdSet && isResult) {
+      processWorkflowResult(resp.doc.doc.workflowId);
+    }
+     if (isWorkflowIdSet && isWorkflow) {
+      generateWorkflowHeaders(resp.doc.doc.workflowId);
+    }
+    if (!isWorkflowIdSet && isResult) {
+      processAssessmentResult(resp.doc.doc.assessmentId);
+    }
+    if (isAssessment || isCurriculum || isQuestion || isSubtest) {
+      generateAssessmentHeaders(resp.doc.doc.assessmentId);
+    }
+    res.json(resp);
   });
+
+  feed.on('error', (err) => res.send(Error(err)));
 
   feed.follow();
 
-  process.nextTick(function () {
-    let ans = Math.random() * 2000;
-    TMP_TANGERINE.insert({ [`mel-${ans}`]: "baz"},  `mel-${ans}`);
+  // TODO: Remove this before life testing
+  process.nextTick( () => {
+    sampleDoc._id = sampleDoc._id + Math.floor(Math.random() * 2000);
+    TMP_TANGERINE.insert({ doc: sampleDoc },  sampleDoc._id, (err, body) => {
+      console.log('saved ====::', body);
+    });
   });
 }
 
+// TODO: Decide to keep or remove this
 function retrieveChangeDoc(docId) {
   return new Promise((resolve, reject) => {
     TMP_TANGERINE.get(docId, (err, body) => {
